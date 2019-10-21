adc5f6b292c911e35dee20a85454807e1b458079
Benjamin Marquis <bmarquiscom@gmail.com>
2019-10-20 22:07:34 -0400

    add zappier integration and spinner

--- src/components/header/HeaderForm.jsx
+++ src/components/header/HeaderForm.jsx
@@ -1,8 +1,64 @@
-import React from 'react';
+import React, { Fragment, useState, useEffect, useRef } from 'react';
+import Spinner from '../Spinner';
 
 const HeaderForm = () => {
+  const [email, setEmail] = useState("");
+  const [sending, setSending] = useState(false);
+  const [succes, setsucces] = useState(false);
+
+  const handleChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    console.log(process.env.ZAPPIER_ENDPOINT);
+    setSending(true);
+    setTimeout(() => {
+      fetch(process.env.ZAPPIER_ENDPOINT, {
+        method: 'POST',
+        body: JSON.stringify({ email }),
+      }).then(() => {
+        setSending(false);
+        setsucces(true);
+      })
+        .catch(() => alert("There was an error, please try again"));
+    }, 3000);
+    setEmail("");
+  };
+
+  const emailBodyContent = () => {
+    if (succes) {
+      return <h2 className="emailSucces">Merci, on vous contact prochainement!</h2>;
+    } else if (sending) {
+      return <Spinner />;
+    }
+    return (
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="emailInput">Email address</label>
+          <input
+            ref={emailInput => emailInput && emailInput.focus()}
+            type="email"
+            required
+            className="form-control"
+            id="emailInput"
+            value={email}
+            placeholder="Laissez nous votre email"
+            onChange={handleChange}
+          />
+        </div>
+        <button type="submit" className="btn btnVizz">Submit</button>
+      </form>
+    );
+  };
+
   return (
-    <h2>Participez au devellopement de VizzMD</h2>
+    <Fragment>
+      <h2 style={{ marginBottom: "10px" }}>Participez au devellopement de VizzMD</h2>
+      {emailBodyContent()}
+    </Fragment>
   );
 };
 

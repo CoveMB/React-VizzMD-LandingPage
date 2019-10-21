6e769dba8ff7557bdee1390ccca88a7f0f31ce90
Benjamin Marquis <bmarquiscom@gmail.com>
2019-10-21 08:35:02 -0400

    add feature image

--- src/components/cards/CardsContainer.jsx
+++ src/components/cards/CardsContainer.jsx
@@ -35,8 +35,8 @@ const CardsContainer = () => {
 
   const selectCard = (topic) => {
     setSelectedCard({ isSelected: true, selected: topic });
-    document.body.scrollTop = 885;
-    document.documentElement.scrollTop = 885;
+    document.body.scrollTop = 950;
+    document.documentElement.scrollTop = 950;
   };
 
   const closeSelectedCard = () => {

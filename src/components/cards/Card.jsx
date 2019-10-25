3d1c105be7c539d60fc5f03c19ba9254e6fcfa6b
Benjamin Marquis <bmarquiscom@gmail.com>
2019-10-25 16:19:56 -0400

    changed cards for topic

--- src/components/cards/Card.jsx
+++ src/components/cards/Card.jsx
@@ -1,22 +1,104 @@
 import React from 'react';
+import Plx from 'react-plx';
 
 const card = (props) => {
-  const handleClick = () => {
-    props.selectCard(props.topic);
+  const parallaxDataPT = [
+    {
+      start: "self",
+      duration: 300,
+      easing: "linear",
+      properties: [
+        {
+          startValue: 280,
+          endValue: 0,
+          property: 'translateY',
+        },
+      ],
+    },
+  ];
+
+  const parallaxDataPE = [
+    {
+      start: "self",
+      duration: 500,
+      easing: "linear",
+      properties: [
+        {
+          startValue: 280,
+          endValue: 0,
+          property: 'translateY',
+        },
+      ],
+    },
+  ];
+
+  const parallaxDataOM = [
+    {
+      start: "self",
+      duration: 700,
+      easing: "linear",
+      properties: [
+        {
+          startValue: 280,
+          endValue: 0,
+          property: 'translateY',
+        },
+      ],
+    },
+  ];
+
+  const getParallaxData = (topic) => {
+    switch (topic) {
+      case "personalised":
+        return parallaxDataPT;
+      case "engagement":
+        return parallaxDataPE;
+      case "mesure":
+        return parallaxDataOM;
+      default:
+        return parallaxDataPT;
+    }
   };
 
+  const cardText = () => {
+    switch (props.topic) {
+      case "personalised":
+        return (
+          <p className="cardText">
+              Personnaliser la thérapie a le potentiel d'assurer de meilleurs soins, adaptés aux patients avec une approche holistique et si nécessaire multidisciplinaire. elle permet aussi <strong className="blueTitle">d'améliorer les soins de santé tout en réduisant les coûts.</strong>
+          </p>
+        );
+      case "engagement":
+        return (
+          <p className="cardText">
+            L’engagement accru du patient dans ses soins de santé contribue à en améliorer les résultats. Les patients proactifs qui sont impliqués dans leurs traitements ont tendance à avoir une <strong className="blueTitle">progression plus rapide.</strong>
+          </p>);
+      case "mesure":
+        return (
+          <p className="cardText">
+            Une meilleure mesure des résultats des traitements administré permet de prendre des décisions plus éclairées sur les soins à attribuer aux patients. Ainsi que <strong className="blueTitle">détablire un lien de confiance entre traitment et cout.</strong>
+          </p>
+        );
+      default:
+        return null;
+    }
+  };
   return (
     <div
-      className="card"
+      className={props.topic === "mesure" ? "topic topicNoBorder mobilepaddingNone" : "topic"}
     >
-      <img className="svgCard" src={`/static/images/svg${props.topic}.svg`} alt={`svg ${props.topic}`} />
-      {props.cardTitle(props.topic)}
-      <button
-        onClick={handleClick}
-        className="normalizeBtn"
+      <Plx
+        parallaxData={getParallaxData(props.topic)}
       >
-        <img className="svgDots" src="/static/images/svgDots.svg" alt="svg dots" />
-      </button>
+        <div className="cardTitleBlock">
+          <img className="svgCard" src={`/static/images/svg${props.topic}.svg`} alt={`svg ${props.topic}`} />
+          {props.cardTitle(props.topic)}
+        </div>
+        <div className="cardMobileTextLayout">
+
+          {cardText()}
+        </div>
+      </Plx>
     </div>
   );
 };
--- src/components/cards/CardsContainer.jsx
+++ src/components/cards/CardsContainer.jsx
@@ -1,11 +1,7 @@
-import React, { Fragment, useState } from 'react';
-import { Animated } from "react-animated-css";
-import SelectedCard from './SelectedCard';
+import React, { Fragment } from 'react';
 import Card from './Card';
 
-const cardsContainer = (props) => {
-  const [selectedCard, setSelectedCard] = useState({ isSelected: false, selected: false });
-
+const cardsContainer = () => {
   const cardTitle = (topic) => {
     switch (topic) {
       case "personalised":
@@ -13,53 +9,19 @@ const cardsContainer = (props) => {
       case "engagement":
         return <h2 className="cardTitle">Engagement Patient</h2>;
       case "mesure":
-        return <h2 className="cardTitle">"Outcome Mesure"</h2>;
+        return <h2 className="cardTitle">Mesure de résultats</h2>;
       default:
         return null;
     }
   };
 
-  const getClassSelected = () => {
-    if (!selectedCard.isSelected) {
-      return "displayNone";
-    }
-    return "cardBox makeItFlex";
-  };
-
-  const getClassCards = () => {
-    if (selectedCard.isSelected) {
-      return "displayNone";
-    }
-    return "cardBox makeItFlex";
-  };
-
-  const selectCard = (topic) => {
-    setSelectedCard({ isSelected: true, selected: topic });
-    props.scrollTo("cards");
-  };
-
-  const closeSelectedCard = () => {
-    setSelectedCard({ isSelected: false, selected: false });
-  };
-
   return (
     <Fragment>
-      <Animated animationIn="fadeIn" animationOut="fadeOut" isVisible={selectedCard.isSelected}>
-        <div className={getClassSelected()}>
-          <SelectedCard
-            topic={selectedCard.selected}
-            closeSelectedCard={closeSelectedCard}
-            cardTitle={cardTitle}
-          />
-        </div>
-      </Animated>
-      <Animated animationIn="fadeIn" animationOut="fadeOut" isVisible={!selectedCard.isSelected}>
-        <div className={getClassCards()}>
-          <Card topic="personalised" selectCard={selectCard} cardTitle={cardTitle} />
-          <Card topic="engagement" selectCard={selectCard} cardTitle={cardTitle} />
-          <Card topic="mesure" selectCard={selectCard} cardTitle={cardTitle} />
-        </div>
-      </Animated>
+      <div className="cardBox makeItFlex flexColumnMobile">
+        <Card topic="personalised" cardTitle={cardTitle} />
+        <Card topic="engagement" cardTitle={cardTitle} />
+        <Card topic="mesure" cardTitle={cardTitle} />
+      </div>
     </Fragment>
   );
 };
